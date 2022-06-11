717b2e08422fb47f50ec3e21ec4925260760ce20
tribber93 <tribberyoni5@gmail.com>
2022-06-11 14:52:40 +0700

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tugas_flutter/main.dart';
+import 'package:tugas_flutter/state_flutter.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
